6c3f3715a86d8a5711aa81122684b2593e1f6fcd
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-10-08 19:29:55 -0500

    Found more accurate API with easy to use key and decent free usage, put together a small amount of logic to be refactored into methods

--- README.md
+++ README.md
@@ -1,21 +1,14 @@
 # CLI Weather
 
-## Gems
+## Project Dependencies
 
-httparty: [https://github.com/jnunemaker/httparty](https://github.com/jnunemaker/httparty)
+httparty (gem): [https://github.com/jnunemaker/httparty](https://github.com/jnunemaker/httparty)  
+
+positionstack (API): [https://positionstack.com/](https://positionstack.com/)
 
 ## Snippets
-https://opencagedata.com/api
-Bloomington
-https://forecast.weather.gov/MapClick.php?lat=44.829198159239716&lon=-93.34481371206564#.X3vAoGhKhPY
+National Weather Service URL parms
+```https://forecast.weather.gov/MapClick.php?lat=<latitude value>&lon=<longitude value>&unit=0&lg=english&FcstType=digital```
 
+Bloomington MN
 https://forecast.weather.gov/MapClick.php?lat=44.8292&lon=-93.3448&unit=0&lg=english&FcstType=digital
-
-https://forecast.weather.gov/MapClick.php?lat=43.1566&lon=77.6088&unit=0&;g=english&FcstType=digital
-43.1566° N, 77.6088° W
-
-41.374775
--83.651321
-
-BG
-https://forecast.weather.gov/MapClick.php?lat=41.3748&lon=-83.6513&unit=0&lg=english&FcstType=digital
--- lib/geolocation.rb
+++ lib/geolocation.rb
@@ -2,9 +2,33 @@ require 'httparty'
 
 class Geolocation
 
-    response = HTTParty.get('https://api.opencagedata.com/geocode/v1/json?key=d9fd2e09691b4c60b9735145cb396a8a&q=West%20Bloomington+1%2C+99423+Minnesota%2C+United%20States&pretty=1')
-    response_json = JSON.parse(response.body)
+    # How can this be hidden from GitHub?
+    API_KEY = "c945744d9d15f2e14ff811ff3900a645"
 
-    puts response_json["results"][2]["geometry"]
+    # Add initialize to get address
+    # Add method to create proper string for API
+    address = "5032 Nine Mile Creek Parkway, Bloomington MN"
+
+    # Add method to get back API response, parse to JSON and return
+    url = "http://api.positionstack.com/v1/forward?access_key=#{API_KEY}&query=#{address}"
+
+    response = HTTParty.get(url)
+
+    response_json = JSON.parse(response.body)
+    
+    puts response_json["data"]
+
+    # Move link portion to Forecast Scraper class
+    puts "https://forecast.weather.gov/MapClick.php?lat=#{geo_latitude}&lon=#{geo_longitude}&unit=0&lg=english&FcstType=digital"
+
+    # Add method to create this hash from the response - iterate if more than 1 set of return values or always use first?
+    geo_data = {
+        :latitude => response_json["data"][0]["latitude"],
+        :longitude => response_json["data"][0]["longitude"],
+        :city => response_json["data"][0]["locality"],
+        :state => response_json["data"][0]["region_code"]
+    }
+
+    puts geo_data
 
 end
