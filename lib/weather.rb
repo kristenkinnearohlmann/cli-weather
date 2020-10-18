454ced93ec09752027962c5e8524dafb747451b8
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-10-17 19:36:24 -0500

    Adding to structure of weather class

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,7 +1,12 @@
 class Weather
 
+    def initialize()
+        puts "Initialize"
+    end
+
     def self.start()
         puts "Welcome to CLI Weather!"
+        weather = self.new
     end
 
 end
