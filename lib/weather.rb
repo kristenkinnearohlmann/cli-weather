46aceccd97d4e468871456de22dc0ba508c48240
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-10-20 20:46:48 -0500

    Refined initial test to include both puts statements

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,6 +2,7 @@ class Weather
 
     def initialize()
         puts "Let's get the weather!"
+        puts "Enter a location (zipcode or city, state zipcode) to find some weather: "
     end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,8 +1,9 @@
 describe Weather do
-    context 'Initialize application' do
-        it 'Greets the user' do
+    context 'Start application' do
+        it 'Greets the user and asks for input' do
             # #puts always includes a newline character that needs to be tested for
-            expect { Weather.new }.to output("Let's get the weather!\n").to_stdout
+            expect {Weather.new}.to output("Let's get the weather!\nEnter a location (zipcode or city, state zipcode) to find some weather: \n").to_stdout
         end
+
     end
 end
