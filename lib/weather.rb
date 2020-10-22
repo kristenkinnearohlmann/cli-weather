82d173df036899c030cc197be3fcd6b8c6821dd1
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-10-21 20:19:39 -0500

    Added more to weather class, considering a refactor for more methods

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,8 +1,13 @@
 class Weather
 
+    attr_accessor :address, :response
+
     def initialize()
         puts "Let's get the weather!"
         puts "Enter a location (zipcode or city, state zipcode) to find some weather: "
+        @address = gets.chomp
+        puts "You entered #{@address}, is this correct? Enter Y or N"
+        @response = gets.chomp
     end
 
 end
