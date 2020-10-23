5918bdcdf58a26342e58c35e95bf383a328fe657
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-10-23 18:23:13 -0500

    Built #get_address, need to review

--- lib/weather.rb
+++ lib/weather.rb
@@ -13,10 +13,27 @@ class Weather
     end
 
     def get_address
-        puts "Enter a location (zipcode or city, state zipcode) to find some weather: "
-        @address = gets.chomp
-        puts "You entered #{@address}, is this correct? Enter Y or N"
-        @response = gets.chomp
+        @response = "N"
+
+        while @response == "N"
+            puts "Choose location type to enter - 1. Zip code only or 2. City, State, Zip code"
+            addr_type_sel = gets.chomp.to_i
+
+            if addr_type_sel == 1 then
+                puts "Enter zipcode: "
+                @address = gets.chomp
+            elsif addr_type_sel == 1 then
+                puts "Enter address in the format city, state zipcode: "
+                @address = gets.chomp
+            end
+
+            puts "You entered #{@address}, is this correct? Enter Y or N"
+            @response = gets.chomp
+
+            if @response == 'Y' then
+                puts "Let's get some weather!"
+            end
+        end
     end
 
 end
