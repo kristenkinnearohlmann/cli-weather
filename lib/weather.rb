ea45c78d3e595dd1a3220397953363783d0f9c7a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-10-22 20:15:05 -0500

    Refactored into more methods

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,8 +2,17 @@ class Weather
 
     attr_accessor :address, :response
 
-    def initialize()
+    def initialize
         puts "Let's get the weather!"
+        get_weather
+    end
+
+    def get_weather
+        get_address
+        puts "End of #get_weather"
+    end
+
+    def get_address
         puts "Enter a location (zipcode or city, state zipcode) to find some weather: "
         @address = gets.chomp
         puts "You entered #{@address}, is this correct? Enter Y or N"
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,8 +1,8 @@
 describe Weather do
-    context 'Start application' do
-        it 'Greets the user and asks for input' do
+    context '#initalize' do
+        it 'Greets the user' do
             # #puts always includes a newline character that needs to be tested for
-            expect {Weather.new}.to output("Let's get the weather!\nEnter a location (zipcode or city, state zipcode) to find some weather: \n").to_stdout
+            expect {Weather.new}.to output("Let's get the weather!\n").to_stdout
         end
 
     end
