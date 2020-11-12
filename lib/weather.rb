cf3413b3f358459ea83be4e843bc37b8bd7b6584
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-11-11 20:17:23 -0600

    Formatted initial greeting and menu

--- lib/weather.rb
+++ lib/weather.rb
@@ -4,7 +4,7 @@ class Weather
     attr_reader :quit
 
     def initialize
-        puts "Let's get the weather!"
+        puts "\nLet's get the weather!"
         @quit = false
         weather_main
     end
@@ -23,21 +23,21 @@ class Weather
         response = 'n'
 
         while (@address_type != 0 && response == 'n')
-            print "Choose location type to enter:\n\t[1] Zip code only\n\t[2] City, State\n\t[3] Full Address\n\t[0] Quit\nEnter your choice: "
+            print "\nChoose location type to enter:\n\t[1] Zip code only\n\t[2] City, State\n\t[3] Full Address\n\t[0] Quit\nEnter your choice: "
             @address_type = gets.chomp.to_i
 
             if address_type == 0 then # quit
-                puts "Have a great day!"
+                puts "\nHave a great day!"
                 @quit = true
                 break
             elsif @address_type == 1 then # zipcode only
-                print "Enter zipcode: "
+                print "\nEnter zipcode: "
                 @address = gets.chomp
             elsif @address_type == 2 then # city & state
-                print "Enter city, state: "
+                print "\nEnter city, state: "
                 @address = gets.chomp
             elsif @address_type == 3 then # full address
-                print "Enter full address (street, city, state, zipcode): "
+                print "\nEnter full address (street, city, state, zipcode): "
                 @address = gets.chomp
             end
 
@@ -45,7 +45,7 @@ class Weather
             response = gets.chomp.downcase
 
             if response == 'y' then
-                puts "Let's get some weather!"
+                puts "\nLet's get some weather!"
             end
         end
     end
