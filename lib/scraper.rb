7dfd373eeafd591949ef954c68f791333670da16
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-11-15 20:24:55 -0600

    Added #self.get_data to begin data retrieval from URL

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,11 +2,19 @@ class Scraper
     # Nokogiri
     # Open URL
 
+    attr_reader :url
+
     def self.nws(geo_location)
         lat_val = geo_location.geo_data[:latitude]
         lon_val = geo_location.geo_data[:longitude]
 
-        puts "https://forecast.weather.gov/MapClick.php?lat=#{lat_val}&lon=#{lon_val}&unit=0&lg=english&FcstType=digital"
-        
+        @url = "https://forecast.weather.gov/MapClick.php?lat=#{lat_val}&lon=#{lon_val}&unit=0&lg=english&FcstType=digital"
+
+        self.get_data
     end
+
+    def self.get_data
+        puts @url
+    end
+
 end
