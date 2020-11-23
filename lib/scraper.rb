3e25833cbc1b3980a8576dbfb30813771382615a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-11-22 20:18:34 -0600

    Wrote basic code to output conditions

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,10 +30,15 @@ class Scraper
         #       condition_details = @url_data.css("#current-conditions").css("#current_conditions_detail").css("td").children
         #       condition_arr = []
         #       condition_details.each do |item|
-        #           condition_arr << val
+        #           condition_arr << item.text.strip
         #       end
         #       need to use puts to display the value correctly
         #       this works but how to iterate?? puts "#{condition_details[6].text}: #{condition_details[7].text}"
+        #       condition_arr.each_with_index do |val,index|
+        #           print "#{val}: " if index % 2 == 0
+        #           puts "#{val}" if index % 2 == 1
+        #       end
+        end
         # 7 Day Forecast (in progress): 
         #   Title: @url_data.css("#seven-day-forecast").css(".panel-title").text.strip
     end
