34c019ba93981a56fb909e608d8d3a0f0d7a12da
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-12-14 20:50:50 -0600

    More work on final output of weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -58,8 +58,17 @@ class Weather
     def print_weather
         puts "\nWeather from #{@weather_summary[:source]} (#{@weather_summary[:url]})."
         puts "#{@weather_summary[:wx_rpt_location][:loc_title]} (Lat: #{@weather_summary[:wx_rpt_location][:lat]} Lon: #{@weather_summary[:wx_rpt_location][:lon]} Elev: #{@weather_summary[:wx_rpt_location][:elev]})"
-        puts "\nCurrent Conditions: #{@weather_summary[:current_conditions]}"
+
+        puts "\nCurrent Conditions:"
+        puts "#{@weather_summary[:current_conditions][:summary]} (#{@weather_summary[:current_conditions][:temp_farenheit]}|#{@weather_summary[:current_conditions][:temp_celsius]})"
+        @weather_summary[:current_conditions].each do |item,val|
+            if ((item != :summary) && (item != :temp_farenheit) && (item != :temp_celsius))
+                puts "#{item.capitalize()}: #{val}"
+            end
+        end
+
         puts "\nDetailed Forecast: #{@weather_summary[:detailed_forecast]}"
+        binding.pry
     end
 
 end
