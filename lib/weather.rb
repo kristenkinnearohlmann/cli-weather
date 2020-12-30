73adf0a380ff44d98e0f1469869a8072c81c9a7b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-12-29 20:08:33 -0600

    Added another scenario for flow

--- lib/weather.rb
+++ lib/weather.rb
@@ -14,6 +14,7 @@ class Weather
         while !quit?
             # quit? false at start of loop
             # get_address can result in quit? true
+            # get_address can result in No Choice
             # no data in @geo_location.geo_data.empty? should be quit? true
             # returned No Choice from @geo_location - retry
             # DETAIL more scenarios to determine proper conditional
