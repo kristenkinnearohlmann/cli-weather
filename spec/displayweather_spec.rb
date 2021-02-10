6c87be0bddb3823fbe2caf330b58f40aaa32dc5d
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-02-09 20:23:27 -0600

    Adjusted test for DisplayWeather

--- spec/displayweather_spec.rb
+++ spec/displayweather_spec.rb
@@ -2,7 +2,8 @@ describe Weather do
     context '#initalize' do
         it 'Greets the user' do
             # #puts always includes a newline character that needs to be tested for
-            expect {Weather.new}.to output("Let's get the weather!\n").to_stdout
+            forecast = DisplayWeather.new
+            expect {forecast.run}.to output("\nLet's get the weather!\n").to_stdout
         end
 
     end
